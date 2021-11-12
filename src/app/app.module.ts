540b45e25ec6b7ae3081cf95de614168c57da3e6
Krzysztof Ciszak <ciszak.kr@gmail.com>
2021-11-12 21:25:34 +0100

    layout and mocked list of favourite jokes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 
 import { JokeComponent } from './joke/joke.component';
 import { JokeService } from './joke/joke.service';
@@ -18,7 +20,12 @@ import { HeaderComponent } from './header/header.component';
 import { FavouriteJokesComponent } from './favourite-jokes/favourite-jokes.component';
 
 @NgModule({
-  declarations: [AppComponent, JokeComponent, HeaderComponent, FavouriteJokesComponent],
+  declarations: [
+    AppComponent,
+    JokeComponent,
+    HeaderComponent,
+    FavouriteJokesComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -28,6 +35,8 @@ import { FavouriteJokesComponent } from './favourite-jokes/favourite-jokes.compo
     MatSelectModule,
     MatButtonModule,
     MatToolbarModule,
+    MatIconModule,
+    MatGridListModule,
     HttpClientModule,
     FormsModule,
   ],
--- src/app/favourite-jokes/favourite-jokes.component.ts
+++ src/app/favourite-jokes/favourite-jokes.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-favourite-jokes',
   templateUrl: './favourite-jokes.component.html',
-  styleUrls: ['./favourite-jokes.component.css']
+  styleUrls: ['./favourite-jokes.component.css'],
 })
 export class FavouriteJokesComponent implements OnInit {
+  favouriteJokes: string[] = [
+    'President Roosevelt once rode his horse 100 miles. Chuck Norris carried his the same distance in half the time.',
+    "When Chuck Norris was denied an Egg McMuffin at McDonald's because it was 10:35, he roundhouse kicked the store so hard it became a Wendy's.",
+    "Chuck Norris was what Willis was talkin' about.",
+    'Chuck Norris causes the Windows Blue Screen of Death.',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
