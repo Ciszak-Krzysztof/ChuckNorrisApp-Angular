946c773644d0733864cfa4125af84c89841bf8f6
Krzysztof Ciszak <ciszak.kr@gmail.com>
2021-11-12 19:40:41 +0100

    adding header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { JokeComponent } from './joke/joke.component';
 import { JokeService } from './joke/joke.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, JokeComponent],
+  declarations: [AppComponent, JokeComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,6 +26,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
+    MatToolbarModule,
     HttpClientModule,
     FormsModule,
   ],
