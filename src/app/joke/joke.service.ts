fa2275dda417f9d4f23d0d3b27dbea697d8f78c1
Krzysztof Ciszak <ciszak.kr@gmail.com>
2021-11-15 19:25:11 +0100

    moving baseURL to constants

--- src/app/joke/joke.service.ts
+++ src/app/joke/joke.service.ts
@@ -4,22 +4,21 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Joke } from './joke.model';
 import { jokeResult } from './jokeResult.model';
+import { baseURL } from 'src/constants/api';
 
 @Injectable()
 export class JokeService {
-  baseUrl: string = 'https://api.icndb.com/';
-
   constructor(private httpClient: HttpClient) {}
 
   getRandomJoke(): Observable<Joke> {
     return this.httpClient
-      .get<jokeResult>(this.baseUrl + 'jokes/random?escape=javascript')
+      .get<jokeResult>(baseURL + 'jokes/random?escape=javascript')
       .pipe(map((result) => result.value));
   }
 
   getCategories(): Observable<String[]> {
     return this.httpClient
-      .get<any>(this.baseUrl + 'categories')
+      .get<any>(baseURL + 'categories')
       .pipe(map((result) => result.value));
   }
 
@@ -27,13 +26,13 @@ export class JokeService {
     if (category === '') {
       return this.httpClient
         .get<jokeResult>(
-          `${this.baseUrl}jokes/random?firstName=${firstName}&lastName=${lastName}&escape=javascript`
+          `${baseURL}jokes/random?firstName=${firstName}&lastName=${lastName}&escape=javascript`
         )
         .pipe(map((result) => result.value));
     } else {
       return this.httpClient
         .get<jokeResult>(
-          `${this.baseUrl}jokes/random?firstName=${firstName}&lastName=${lastName}&limitTo=${category}&escape=javascript`
+          `${baseURL}jokes/random?firstName=${firstName}&lastName=${lastName}&limitTo=${category}&escape=javascript`
         )
         .pipe(map((result) => result.value));
     }
